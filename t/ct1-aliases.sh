2fc7a2f4cca60061e144f58afb3ffe4f565ddaa4
Armand Leclercq <marman.email@gmail.com>
2015-02-03 21:49:40 +0100

    Change quote to allow a complete test

--- t/ct1-aliases.sh
+++ t/ct1-aliases.sh
@@ -21,12 +21,13 @@ test_expect_success 'Launch alias alias' '
       test_cmp result expect
 '
 
-test_expect_success 'Launch aliases alias' '
+test_expect_success 'Launch aliases alias' "
       cat >expect <<-\EOF &&
-alias = !f () { test $# = 2 && git config --global alias."$1" "$2" && exit 0 || echo "usage: git alias <new alias> <original command>" >&2 && exit 1 - ;}; f
+alias = !f () { test \$# = 2 && git config --global alias.\"\$1\" \"\$2\" && exit 0 || echo \"usage: git alias <new alias> <original command>\" >&2 && exit 1 - ;}; f
+aliases = !git config --get-regexp 'alias.*' | colrm 1 6 | sed 's/[ ]/ = /'
 EOF
       git aliases >result &&
       test_cmp result expect
-'
+"
 
 test_done
