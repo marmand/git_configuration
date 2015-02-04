aa55f6af33ee1fb9171ffbe6793416cb85a5dea4
Armand Leclercq <aleclercq@weborama.com>
2015-02-04 13:57:16 +0100

    Test all checkout aliases

--- t/ct2-checkout.sh
+++ t/ct2-checkout.sh
@@ -10,4 +10,54 @@ test_expect_success 'Setup checkout aliases' '
         configurate_with checkout
 '
 
+printf "  devel\n* master\n" >expect
+test_expect_success 'Simple checkout' '
+        echo one >one &&
+        git add one &&
+        git commit -m "one" &&
+        git checkout -b devel &&
+        echo two >two &&
+        git add two &&
+        git commit -m "two" &&
+        git co master &&
+        git branch >result &&
+        test_cmp result expect
+'
+
+printf "* devel\n  master\n" >expect
+test_expect_success 'Another one' '
+        git co devel &&
+        git branch >result &&
+        test_cmp result expect
+'
+
+test_expect_success 'Setup remote' '
+        git init --bare remote.git &&
+        git remote add origin remote.git
+'
+
+printf "* devel
+  master
+  remotes/origin/tata
+  remotes/origin/titi
+  remotes/origin/toto
+" >expect
+test_expect_success 'Setup branches on remote' '
+        git push origin HEAD:toto &&
+        git push origin HEAD:tata &&
+        git push origin HEAD:titi &&
+        git branch -a > result &&
+        test_cmp result expect
+'
+
+printf "  devel
+  master
+* tata
+" >expect
+test_expect_success 'Test cob alias' '
+        git cob tata &&
+        git branch >result &&
+        test_cmp result expect
+'
+
 test_done
