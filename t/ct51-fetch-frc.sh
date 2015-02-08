ca97be8d3b691f0a4bc429edef7d902ffc17b206
Armand Leclercq <marman.email@gmail.com>
2015-02-08 13:00:10 +0100

    Complete test on multiple clones

--- t/ct51-fetch-frc.sh
+++ t/ct51-fetch-frc.sh
@@ -32,6 +32,56 @@ test_expect_success 'Setup remote repository' '
         test_do_in author_clone test_do_as "Author" git push &&
         git fetch &&
         git k >result &&
+        git nbr toto &&
+        test_commit G &&
+        test_commit H &&
+        test_commit I &&
+        git push &&
+        test_do_in author_clone test_do_as "Author" git pull &&
+        test_do_in author_clone test_do_as "Author" git checkout toto &&
+        test_do_in author_clone test_do_as "Author" test_commit J &&
+        test_do_in author_clone test_do_as "Author" test_commit K &&
+        test_do_in author_clone test_do_as "Author" test_commit L &&
+        test_do_in author_clone test_do_as "Author" test_commit M &&
+        test_do_in author_clone test_do_as "Author" test_commit N &&
+        test_do_in author_clone test_do_as "Author" test_commit O &&
+        test_do_in author_clone test_do_as "Author" git push &&
+        git fetch &&
+        test_cmp result expect
+'
+
+printf "* b100e5c I
+* e351f26 H
+* c1ebd3d G
+* 23f8942 E
+* 4013395 D
+* 35a8500 C
+* d9df450 B
+* 0ddfaf1 A
+" >expect
+test_expect_success 'Check current repository status'  '
+        git k >result &&
+        test_cmp result expect
+'
+
+printf "* 5c6b5f1 O
+* de89121 N
+* c7f9fab M
+* 987d817 L
+* b565aec K
+* 0e4bc46 J
+* b100e5c I
+* e351f26 H
+* c1ebd3d G
+* 23f8942 E
+* 4013395 D
+* 35a8500 C
+* d9df450 B
+* 0ddfaf1 A
+" >expect
+test_expect_success 'Check fetch rebased current repository status'  '
+        git frc &&
+        git k >result &&
         test_cmp result expect
 '
 
